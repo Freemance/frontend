efb3c7e43aedbe08c8b502d8c0926281caed9286
paul <paul@outboss.io>
2021-08-24 07:03:50 -0400

    fix: muiTheme

---
+++ src/styles/theme/overrides/MuiCard.ts
@@ -0,0 +1,13 @@
+export default {
+  root: {
+    background: `
+      linear-gradient(var(--color-bg),
+      var(--color-bg)) padding-box,
+      linear-gradient(to right top, #979fe3, rgb(255 255 255 / 0%)) border-box`,
+    borderRadius: 20,
+    border: '2px solid transparent',
+  },
+  elevation1: {
+    boxShadow: 'none',
+  },
+};
--- src/styles/theme/theme.ts
+++ src/styles/theme/theme.ts
@@ -1,32 +1,15 @@
 import { createTheme, ThemeOptions } from '@material-ui/core';
-
-export const paletteColorsDark = {
-  primary: '#0f4c75',
-  secondary: '#3282b8',
-  error: '#E44C65',
-  background: '#1b262c',
-  text: '#bbe1fa',
-};
-
-export const paletteColorsLight = {
-  primary: '#6886c5',
-  secondary: '#ffe0ac',
-  error: '#E44C65',
-  background: '#f9f9f9',
-  text: '#050505',
-};
+import * as palette from './palettes';
+import overrides from './overrides';
+import { typography } from './typography';
 
 const options = (dark: boolean): ThemeOptions => {
-  const paletteColors = dark ? paletteColorsDark : paletteColorsLight;
   return {
-    palette: {
-      type: dark ? 'dark' : 'light',
-      primary: {
-        main: paletteColors.primary,
-      },
-      // ...
-    },
+    palette: dark ? palette.darkPalette : palette.lightPalette,
+    overrides: overrides,
+    typography: typography,
   };
 };
+
 export const darkTheme = createTheme(options(true));
 export const lightTheme = createTheme(options(false));
