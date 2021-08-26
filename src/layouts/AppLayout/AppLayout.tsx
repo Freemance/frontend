66ea2b2dfc2746f77bc167d5d13612029d3ec8b6
jezsdaniel <jadendaniel7@gmail.com>
2021-08-26 14:57:57 -0400

    Remove unused import from AppLayout

--- src/layouts/AppLayout/AppLayout.tsx
+++ src/layouts/AppLayout/AppLayout.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { Container, Typography } from '@material-ui/core';
+import { Container } from '@material-ui/core';
 import NavBar from '@components/organisms/NavBar';
 import Hero from '@components/organisms/Hero';
 const AppLayout: React.FC = () => {
