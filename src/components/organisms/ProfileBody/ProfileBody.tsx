cee904307bc33aac7942e8a5e7c8f222e604f641
jezsdaniel <jadendaniel7@gmail.com>
2021-08-27 17:07:09 -0400

    Handle change current tab

--- src/components/organisms/ProfileBody/ProfileBody.tsx
+++ src/components/organisms/ProfileBody/ProfileBody.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Divider, Tab, Tabs } from '@material-ui/core';
 
 import { useProfileBodyStyle } from './ProfileBody.style';
@@ -6,14 +6,20 @@ import { useProfileBodyStyle } from './ProfileBody.style';
 const ProfileBody = () => {
   const classes = useProfileBodyStyle();
 
+  const [currentTab, setCurrentTab] = useState(0);
+
+  const handleTabChange = (event: React.ChangeEvent<{}>, newValue: number) => {
+    setCurrentTab(newValue);
+  };
+
   return (
     <div className={classes.contain}>
       <Divider />
-      <Tabs value={0} variant="scrollable">
+      <Tabs value={currentTab} onChange={handleTabChange} variant="scrollable">
         <Tab value={0} label="Projects" />
-        <Tab label="Experience" />
-        <Tab label="Education" />
-        <Tab label="Skills" />
+        <Tab value={1} label="Experience" />
+        <Tab value={2} label="Education" />
+        <Tab value={3} label="Skills" />
       </Tabs>
       <Divider />
     </div>
