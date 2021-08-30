221f0da124ddf5660676ed7243409a208c9426f3
Cristian Fonseca <cfonsecacomas@gmail.com>
2021-08-30 11:11:35 -0400

    CREATE: A Component for description of the section(completly generic)

---
+++ src/components/atoms/DescriptionSection/DescriptionSection.style.ts
@@ -0,0 +1,9 @@
+import { createStyles, Theme, makeStyles } from '@material-ui/core/styles';
+
+export const useDescriptionStyles = makeStyles((theme: Theme) => ({
+  descriptionsection: {
+    display: 'flex',
+    justifyContent: 'center',
+    marginTop: '3%',
+  },
+}));
