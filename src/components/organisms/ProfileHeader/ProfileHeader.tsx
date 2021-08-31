a7b00692e8e33186d520093496199b1f31640ed7
jezsdaniel <jadendaniel7@gmail.com>
2021-08-31 12:04:23 -0400

    Show diferent buttons on Profile if isUser or isEdit

--- src/components/organisms/ProfileHeader/ProfileHeader.tsx
+++ src/components/organisms/ProfileHeader/ProfileHeader.tsx
@@ -3,27 +3,44 @@ import { Grid, IconButton } from '@material-ui/core';
 import {
   Call as CallIcon,
   Description as DescriptionIcon,
+  Edit as EditIcon,
 } from '@material-ui/icons';
 
 import ProfileAvatar from '@components/molecules/ProfileAvatar';
 import ProfileInfo from '@components/molecules/ProfileInfo';
 import { useProfileHeaderStyle } from './ProfileHeader.style';
+import { useProfileContext } from '@layouts/ProfileLayout';
 
 const ProfileHeader = () => {
   const classes = useProfileHeaderStyle();
+
+  const { isUser, isEdit, setIsEdit } = useProfileContext();
 
   return (
     <Grid container spacing={3} justifyContent="center" alignItems="flex-start">
       <Grid item md={3}>
         <ProfileAvatar />
-        <div className={classes.buttonsContainer}>
-          <IconButton className={classes.headerButton}>
-            <CallIcon />
-          </IconButton>
-          <IconButton className={classes.headerButton}>
-            <DescriptionIcon />
-          </IconButton>
-        </div>
+        {!isEdit && (
+          <div className={classes.buttonsContainer}>
+            {isUser ? (
+              <IconButton
+                className={classes.headerButton}
+                onClick={() => setIsEdit(true)}
+              >
+                <EditIcon />
+              </IconButton>
+            ) : (
+              <>
+                <IconButton className={classes.headerButton}>
+                  <CallIcon />
+                </IconButton>
+                <IconButton className={classes.headerButton}>
+                  <DescriptionIcon />
+                </IconButton>
+              </>
+            )}
+          </div>
+        )}
       </Grid>
       <Grid item md={9}>
         <ProfileInfo />
