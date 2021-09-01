ef8286e9860bf0fe555799ecf89ebad903ef60ee
jezsdaniel <jadendaniel7@gmail.com>
2021-09-01 17:34:51 -0400

    Make ProfileSkills editable

--- src/components/molecules/ProfileSkills/ProfileSkills.tsx
+++ src/components/molecules/ProfileSkills/ProfileSkills.tsx
@@ -1,17 +1,35 @@
 import React from 'react';
 import { Chip } from '@material-ui/core';
+import { Add as AddIcon } from '@material-ui/icons';
 
 import { useProfileSkillsStyle } from './ProfileSkills.style';
 import IProfileSkills from './types';
+import { useProfileContext } from '@layouts/ProfileLayout';
 
 const ProfileSkills = ({ skills }: IProfileSkills) => {
   const classes = useProfileSkillsStyle();
 
+  const { isEdit } = useProfileContext();
+
   return (
     <div className={classes.root}>
-      {skills.map((skill, index) => (
-        <Chip key={index} className={classes.chip} label={skill.name} />
-      ))}
+      {isEdit && (
+        <Chip icon={<AddIcon />} label="Add skill" onClick={() => {}} />
+      )}
+      {skills.map((skill, index) =>
+        isEdit ? (
+          <Chip
+            key={index}
+            color="primary"
+            label={skill.name}
+            variant="outlined"
+            onClick={() => {}}
+            onDelete={() => {}}
+          />
+        ) : (
+          <Chip key={index} className={classes.chip} label={skill.name} />
+        )
+      )}
     </div>
   );
 };
