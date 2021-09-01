23997842d2b8efc2984e6181fedc67f10d24ef78
jezsdaniel <jadendaniel7@gmail.com>
2021-09-01 19:06:08 -0400

    Create add and edit skill dialog

--- src/components/molecules/ProfileSkills/ProfileSkills.tsx
+++ src/components/molecules/ProfileSkills/ProfileSkills.tsx
@@ -1,5 +1,13 @@
-import React from 'react';
-import { Chip } from '@material-ui/core';
+import React, { useEffect, useState } from 'react';
+import {
+  Button,
+  Chip,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogTitle,
+  TextField,
+} from '@material-ui/core';
 import { Add as AddIcon } from '@material-ui/icons';
 
 import { useProfileSkillsStyle } from './ProfileSkills.style';
@@ -11,10 +19,30 @@ const ProfileSkills = ({ skills }: IProfileSkills) => {
 
   const { isEdit } = useProfileContext();
 
+  const [open, setOpen] = useState<boolean>(false);
+  const [dialogTitle, setDialogTitle] = useState<String>('');
+
+  useEffect(() => {
+    if (dialogTitle != '') {
+      setOpen(true);
+    }
+  }, [dialogTitle]);
+
+  const handleClose = () => {
+    setDialogTitle('');
+    setOpen(false);
+  };
+
   return (
     <div className={classes.root}>
       {isEdit && (
-        <Chip icon={<AddIcon />} label="Add skill" onClick={() => {}} />
+        <Chip
+          icon={<AddIcon />}
+          label="Add skill"
+          onClick={() => {
+            setDialogTitle('Add skill');
+          }}
+        />
       )}
       {skills.map((skill, index) =>
         isEdit ? (
@@ -23,13 +51,29 @@ const ProfileSkills = ({ skills }: IProfileSkills) => {
             color="primary"
             label={skill.name}
             variant="outlined"
-            onClick={() => {}}
+            onClick={() => {
+              setDialogTitle('Edit skill');
+            }}
             onDelete={() => {}}
           />
         ) : (
           <Chip key={index} className={classes.chip} label={skill.name} />
         )
       )}
+      <Dialog open={open} onClose={handleClose}>
+        <DialogTitle>{dialogTitle}</DialogTitle>
+        <DialogContent>
+          <TextField autoFocus margin="dense" label="Skill" fullWidth />
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleClose} color="primary">
+            Cancel
+          </Button>
+          <Button onClick={handleClose} color="primary">
+            Save
+          </Button>
+        </DialogActions>
+      </Dialog>
     </div>
   );
 };
