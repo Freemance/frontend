76b8d8173e6d695cf1bf3391443da536cee7296f
jezsdaniel <jadendaniel7@gmail.com>
2021-09-11 00:22:43 -0400

    Unify Apollo client and add React i18 dependency

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,14 +4,14 @@ import { ThemeProvider } from '@material-ui/core/styles';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import { ApolloProvider } from '@apollo/client';
 
+import { I18nextProvider } from 'react-i18next';
+import { i18ne } from 'src/translation/i18n';
+import i18next from 'i18next';
+
 import { theme } from '@styles/theme';
 import { useApolloClient } from 'src/lib/apollo/client';
 import { GlobalProvider } from 'src/context';
 
-// Internacionalizacion
-import { I18nextProvider } from 'react-i18next';
-import { i18ne } from 'src/translation/i18n';
-import i18next from 'i18next';
 function MyApp({ Component, pageProps }: AppProps) {
   i18ne();
   useEffect(() => {
@@ -23,7 +23,7 @@ function MyApp({ Component, pageProps }: AppProps) {
 
   return (
     <I18nextProvider i18n={i18next}>
-      <ApolloProvider client={Client}>
+      <ApolloProvider client={useApolloClient()}>
         <GlobalProvider>
           <ThemeProvider theme={theme.lightTheme}>
             <CssBaseline />
--- src/lib/apollo/connection.tsx
+++
@@ -1,5 +0,0 @@
-import { InMemoryCache, ApolloClient } from '@apollo/client';
-export const client = new ApolloClient({
-  uri: 'https://freemance-backend2.herokuapp.com/graphql',
-  cache: new InMemoryCache(),
-});
