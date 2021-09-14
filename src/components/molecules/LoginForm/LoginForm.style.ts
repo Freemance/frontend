40dd07bd4b1a01b11ff5771f13c67a9d7fc627c4
Cristian Fonseca <cfonsecacomas@gmail.com>
2021-09-14 15:31:03 -0400

    FA-51: Modify the style in loginform for the responsive view

--- src/components/molecules/LoginForm/LoginForm.style.ts
+++ src/components/molecules/LoginForm/LoginForm.style.ts
@@ -5,6 +5,9 @@ export const useLoginFormStyle = makeStyles((theme: Theme) =>
     form: {
       width: '520px',
       marginTop: theme.spacing(1),
+      [theme.breakpoints.down('xs')]: {
+        width: '100%',
+      },
     },
     alert: {
       width: '100%',
