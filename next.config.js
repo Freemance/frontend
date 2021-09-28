cb091c9d191908063d5c6d447c50e3778a255e33
jezsdaniel <jadendaniel7@gmail.com>
2021-09-28 15:01:03 -0400

    Use env links

--- next.config.js
+++ next.config.js
@@ -1,3 +1,11 @@
 module.exports = {
   reactStrictMode: true,
+  env: {
+    IMAGE_LINK: 'https://freemance-backend.herokuapp.com/uploads/',
+    API_LINK: 'https://freemance-backend.herokuapp.com/graphql',
+    NEXT_PUBLIC_GA_ID: '',
+  },
+  images: {
+    domains: ['freemance-backend.herokuapp.com'],
+  },
 };
