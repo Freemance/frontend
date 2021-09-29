ab792ecb6aaa7d425f14bb8dfd447d6a20125ce4
Freemance Team <team@freemance.com>
2021-09-29 06:16:20 +0000

    fix:change hero poster

--- src/components/organisms/Hero/Hero.tsx
+++ src/components/organisms/Hero/Hero.tsx
@@ -50,7 +50,7 @@ const Hero = () => {
         )}
       </Grid>
       <Grid item xs={12} lg={8} md={12} xl={8} sm={12}>
-        <img src="/mockup.png" className={classes.mockup} />
+        <img src="/mockup.svg" className={classes.mockup} />
       </Grid>
     </Grid>
   );
