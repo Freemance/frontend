2a6437b9faae6f4fb0caba12924347981b7a198e
jezsdaniel <jadendaniel7@gmail.com>
2022-06-24 22:43:59 -0400

    Change faq questions

--- src/components/molecules/FAQContainer/FAQContainer.tsx
+++ src/components/molecules/FAQContainer/FAQContainer.tsx
@@ -2,17 +2,12 @@ import React from 'react';
 import FAQ from '@components/atoms/FAQ/FAQ';
 import { useFAQContainerStyles } from './FAQContainer.style';
 import { useTranslation } from 'react-i18next';
-import { Typography } from '@material-ui/core';
 const FAQContainer = () => {
   const [t] = useTranslation('landpage');
   const classes = useFAQContainerStyles();
   return (
     <>
       <div className={classes.container}>
-        <Typography variant="h2" className={classes.ancla}>
-          <span className={classes.fizzer}>|</span>
-          {t('faq.ancla1')}
-        </Typography>
         <FAQ question={t('faq.question1')} answer={t('faq.answer1')} />
         <FAQ question={t('faq.question2')} answer={t('faq.answer2')} />
         <FAQ question={t('faq.question3')} answer={t('faq.answer3')} />
@@ -21,16 +16,13 @@ const FAQContainer = () => {
         <FAQ question={t('faq.question6')} answer={t('faq.answer6')} />
         <FAQ question={t('faq.question7')} answer={t('faq.answer7')} />
         <FAQ question={t('faq.question8')} answer={t('faq.answer8')} />
-        <Typography variant="h2" className={classes.ancla}>
-          <span className={classes.fizzer}>|</span>
-          {t('faq.ancla2')}
-        </Typography>
-        <div id="mvs">
-          <FAQ
-            question={t('faq.question1-ancla2')}
-            answer={t('faq.answer1-ancla2')}
-          />
-        </div>
+        <FAQ question={t('faq.question9')} answer={t('faq.answer9')} />
+        <FAQ question={t('faq.question10')} answer={t('faq.answer10')} />
+        <FAQ question={t('faq.question11')} answer={t('faq.answer11')} />
+        <FAQ
+          question={t('faq.question1-ancla2')}
+          answer={t('faq.answer1-ancla2')}
+        />
       </div>
     </>
   );
